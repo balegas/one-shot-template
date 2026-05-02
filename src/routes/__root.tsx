8777c3632a8810839cacaedf71cccff9d9bd8728
Valter Balegas <balegas@electric-sql.com>
2026-05-02 11:24:26 +0100

    chore: stop hardcoding dark mode
    The template now defaults to light. Add the .dark class on <html> at
    runtime to opt into dark mode.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -14,7 +14,7 @@ export const Route = createRootRoute({
 
 function RootDocument({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <head>
         <HeadContent />
       </head>
